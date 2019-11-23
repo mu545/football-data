429c899ff6b086a49a660cf59745aece957edd9f
musa sutisna <mus4.sutisn4@gmail.com>
2019-11-23 17:18:52 +0700

    All competitions

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,3 +1,6 @@
-module.exports = function (router) {
+const competitions = require('./v1/competitions.js')
 
+module.exports = function (router) {
+  router.route('/api/v1/competitions')
+    .get(competitions.getList)
 }
---
+++ routes/apis/v1/competitions.js
@@ -0,0 +1,70 @@
+var http = require('http')
+var { param, validationResult } = require('express-validator')
+
+function validationInput(req, res, next) {
+  let errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ err: errors.array({ onlyFirstError: true }) })
+  } else {
+    next()
+  }
+}
+
+function grabCompetitions(req, res, next) {
+  let filters = ''
+
+  for (var filter in res.locals.input) {
+    filters += `${filter}${res.locals.input[filter]}`
+  }
+
+  let options = {
+    method: 'GET',
+    hostname: 'api.football-data.org',
+    port: null,
+    path: `/v2/${filters}`,
+    headers: {
+      'X-AUTH-TOKEN': process.env.FOOTBALL_API_KEY
+    }
+  }
+
+  let fetchCompetitions = http.request(options, function (getCompetitions) {
+    let chunks = []
+
+    getCompetitions.on('data', function (chunk) {
+      chunks.push(chunk)
+    })
+
+    getCompetitions.on('end', function () {
+      res.locals.competitions = JSON.parse(Buffer.concat(chunks))
+
+      if (typeof res.locals.competitions.error !== 'undefined') {
+        res.status(400).json({ err: res.locals.competitions.error })
+      } else {
+        next()
+      }
+    })
+  })
+  fetchCompetitions.on('error', function(err) {
+    res.status(400).json({ err: err.message })
+  })
+  fetchCompetitions.end()
+}
+
+function getListInput(req, res, next) {
+  res.locals.input = {
+    '/competitions': ''
+  }
+
+  next()
+}
+
+module.exports.getList = [
+  getListInput,
+  grabCompetitions,
+  function (req, res) {
+    res.json({
+      competitions: res.locals.competitions
+    })
+  }
+]
